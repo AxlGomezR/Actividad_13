4de66e8440aeca1bbd87c5e971f59f3a96e4a361
ALUMNO <ALUMNO@DESKTOP-F264FR0>
2026-03-17 08:37:54 -0600

    Creación de los métodos CalcularPromedio, Resultado y MostrarInfo

--- poo1/poo1/Program.cs
+++ poo1/poo1/Program.cs
@@ -5,5 +5,26 @@
     double nota2;
     double nota3;
 
-
+    public double CalcularPromedio()
+    {
+        return (nota1 + nota2 + nota3) / 3;
+    }
+    public string Resultado()
+    {
+        if (CalcularPromedio() > 60)
+        {
+            return "Aprobado";
+        }
+        else
+        {
+            return "Reprobado";
+        }
+    }
+    public void MostrarInfo()
+    {
+        Console.WriteLine($"Nombre: {nombre}");
+        Console.WriteLine($"Nota 1: {nota1} | Nota 2: {nota2} | Nota 3: {nota3}");
+        Console.WriteLine($"Promedio: {CalcularPromedio()}");
+        Console.WriteLine($"Resultado: {Resultado()}\n");
+    }
 }
