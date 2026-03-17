0c09045f1d0a09f85c89c9c00e0dc17b8a8a9d78
ALUMNO <ALUMNO@DESKTOP-F264FR0>
2026-03-17 09:00:08 -0600

    Creación de los métodos PrecioTotal, Existencias y MostrarInfo

--- poo2/poo2/Program.cs
+++ poo2/poo2/Program.cs
@@ -4,5 +4,30 @@
     public double precio;
     public int cantidad;
 
-
+    public double PrecioTotal()
+    {
+        return precio * cantidad;
+    }
+    public string Existencias()
+    {
+        if (cantidad > 30)
+        {
+            return "Stock suficiente";
+        }else if(cantidad>0 && cantidad < 30)
+        {
+            return "Stock bajo";
+        }
+        else
+        {
+            return "Sin stock";
+        }
+    }
+    public void MostrarInfo()
+    {
+        Console.WriteLine($"Nombre del producto: {nombre}");
+        Console.WriteLine($"Precio unitario: {precio}");
+        Console.WriteLine($"Número de existencias: {cantidad}");
+        Console.WriteLine($"Precio total: {PrecioTotal()}");
+        Console.WriteLine($"Estado de stock: {Existencias()}\n");
+    }
 }
